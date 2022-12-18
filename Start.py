9cdab695dc6a533de3b11abc17cb4ef932dc074a
Lonit <ridak00790@gamil.com>
2022-12-18 11:20:21 +0200

    [UPD] Add calc

---
+++ Start.py
@@ -0,0 +1,43 @@
+def add(n=0, m=0):
+    return n + m
+
+
+def subs(n=0, m=0):
+    return n - m
+
+
+def prod(n=0, m=0):
+    return n * m
+
+
+def div(n=1, m=0):
+    return n / m
+
+
+def rem(n=1, m=0):
+    return n % m
+
+
+def operations(op, n, m):
+    try:
+        n = int(n)
+        m = int(m)
+    except ValueError:
+        return "Ошибка!\n" \
+               "Введите числа корректно"
+
+    switch = {
+        '+': add(n, m),
+        '-': subs(n, m),
+        '*': prod(n, m),
+        '/': div(n, m),
+        '%': rem(n, m),
+    }
+    return switch.get(op, 'Ошибка!\nВведите оператор корректно: +,-,*,/,%')
+
+
+a = input('Первое число:\t')
+b = input('Второе число:\t')
+o = input('Введите один из операторов (+,-,*,/,%):\t')
+x = operations(o, a, b)
+print(f'{a} {o} {b} = {x}')
